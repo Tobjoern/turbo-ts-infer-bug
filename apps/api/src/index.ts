fafd4df3f02935ba6e619168d5454deca3393df1
Tobjoern <Tobi11111@gmx.de>
2023-08-21 11:36:48 +0200

    Added: Buggy Code

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,5 +1,37 @@
 import { createServer } from "./server";
 import { log } from "logger";
+import { transformer } from "trpc-transformer";
+
+
+type CreateContextOptions = {
+  req: any;
+  res: any;
+  // res: NextApiResponse<any>
+  // req: NextApiRequest
+};
+
+const createInnerTRPCContext = (opts: CreateContextOptions) => {
+  return {
+    // prisma,
+    res: opts.res,
+    req: opts.req,
+    trans: transformer,
+  };
+};
+
+/**
+ * This is the actual context you'll use in your router. It will be used to
+ * process every request that goes through your tRPC endpoint
+ * @link https://trpc.io/docs/context
+ */
+export const createTRPCContext = async (opts: CreateContextOptions) => {
+  const { req, res } = opts;
+
+  return createInnerTRPCContext({
+    res,
+    req,
+  });
+};
 
 const port = process.env.PORT || 5001;
 const server = createServer();
